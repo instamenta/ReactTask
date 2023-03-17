2d2ea822ce0c4dce2737295a23553c89c6963877
instamenta <98179343+instamenta@users.noreply.github.com>
2023-03-17 14:21:43 +0200

    comments added and finishing touches

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,29 +1,29 @@
 
-import './App.css'
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { Catalog } from './components/Catalog/Catalog'
+import './App.css'
 
 function App() {
-  const [ searchValue, setSearchValue] = useState('')
+  const [searchValue, setSearchValue] = useState('')
 
   return (
-    
     <div className="App">
       <nav>
-            <div className="logo">
-                <a href="#">Support Desk</a>
-            </div>
-            <div className="search-box">
-                <input onChange={(e) => {
-                    setSearchValue(e.target.value)
-                    console.log(searchValue)
-                    }} className="search" type="text" placeholder="Search" />
-                <img src="./../../public/icon-search.svg" alt="search-icon" className="nav-icon" />
-            </div>
-        </nav>
-      <Catalog props={searchValue}/>
+        <div className="logo">
+          <a href="#">Support Desk</a>
+        </div>
+        <div className="search-box">
+          {/* Sets the searchValue */}
+          <input onChange={(e) => setSearchValue(e.target.value)}
+            className="search"
+            type="text"
+            placeholder="Search" />
+          <img src="./../../public/icon-search.svg" alt="search-icon" className="nav-icon" />
+        </div>
+      </nav>
+      {/* Passes the setSearchValue to the Catalog component */}
+      <Catalog props={searchValue} />
     </div>
   )
 }
-
 export default App
--- react-app/src/components/Catalog/Catalog.jsx
+++ react-app/src/components/Catalog/Catalog.jsx
@@ -1,13 +1,15 @@
-import dataJSON from '../../api.json'
+import userDataArray from '../../api.json'
 import './Catalog.css'
 
 export const Catalog = ({ props }) => {
-    const catalogList = dataJSON?.map((userData) => {
+    // Maps all the userData and Filters it if needed
+    const catalogList = userDataArray?.map((userData) => {
 
         const { firstName, lastName, available, phone, email, image, location } = userData;
         const fullname = firstName + ' ' + lastName
-
+        // Add data to searchCriteria to filter based on more data
         const searchCriteria = fullname + email
+        // If there is search string value we check if it included in any of our searchCriteria 
         if (props.length > 0 && !searchCriteria.toLowerCase().includes(props.toLowerCase())
         ) return null
 
@@ -15,14 +17,17 @@ export const Catalog = ({ props }) => {
             <div className='card'>
                 <div className='card-image-container'>
                     <img className='card-image' src={image} alt='img'></img>
+                    {/* Render based on the available boolean => Busy or Available Icon */}
                     {available
                         ? <img src="./../../public/icon-available.svg" alt="search-icon" className="card-status-image" />
                         : <img src="./../../public/icon-busy.svg" alt="search-icon" className="card-status-image" />
                     }
                 </div>
                 <div className='card-description-container'>
+                    {/* Displayed fulltime */}
                     <span className='card-fullname'>{fullname}</span>
                     <span className='card-location'>{location}</span>
+                    {/* Hidden class used to hide data if card is not hovered */}
                     <span className='hidden card-phone'>phone: {phone}</span>
                     <span className='hidden card-email'>email: {email}</span>
                 </div>
@@ -31,7 +36,9 @@ export const Catalog = ({ props }) => {
 
     })
     return (
+        // This is where we hold all the cards
         <div className='card-container'>
+            {/* This is where the card list is displayed */}
             {catalogList}
         </div>
     )
