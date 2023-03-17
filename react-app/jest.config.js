6b02730b5b6473dff4828376d490084f49827ac0
instamenta <98179343+instamenta@users.noreply.github.com>
2023-03-18 00:17:23 +0200

    connecting jest and enzyme

--- react-app/jest.config.js
+++ react-app/jest.config.js
@@ -1,11 +1,28 @@
 module.exports = {
     preset: 'babel-jest',
     transform: {
-        '^.+\\.jsx?$': 'babel-jest',
+      '^.+\\.jsx?$': 'babel-jest',
     },
     testEnvironment: 'jsdom',
     moduleNameMapper: {
-        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    }
-
-};
+      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    },
+    testMatch: [
+      '**/__tests__/**/*.js?(x)',
+      '**/?(*.)+(spec|test).js?(x)',
+    ],
+    coverageThreshold: {
+      global: {
+        branches: 80,
+        functions: 80,
+        lines: 80,
+        statements: 80,
+      },
+    },
+    collectCoverageFrom: [
+      'src/**/*.js',
+      '!**/node_modules/**',
+      '!**/vendor/**',
+    ],
+    setupFilesAfterEnv: ['./jest.setup.js'],
+  };
--- react-app/src/components/Catalog/Catalog.test.jsx
+++ react-app/src/components/Catalog/Catalog.test.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import { render } from 'react-test-renderer';
+import { render,screen } from 'react-test-renderer';
 import { Catalog } from './Catalog';
 
+// 1
 describe('Catalog', () => {
     it('renders correctly', () => {
         const component = render(<Catalog />);
         expect(component).toMatchSnapshot();
     });
 });
-import React from 'react';
-import { render } from 'react-test-renderer';
-import { Catalog } from './Catalog';
 
+// 2
 describe('Catalog', () => {
     const userDataArray = [
         {
@@ -39,3 +38,32 @@ describe('Catalog', () => {
         expect(component.toJSON().children).toHaveLength(1);
     });
 });
+// 3
+
+describe('Catalog component', () => {
+    it('renders without errors', () => {
+      render(<Catalog props="" />);
+      expect(screen.getByTestId('catalog-container')).toBeInTheDocument();
+    });
+  
+    it('displays all cards if no search query is provided', () => {
+      render(<Catalog props="" />);
+      expect(screen.getAllByTestId('card')).toHaveLength(4);
+    });
+  
+    it('filters the cards based on the search query', () => {
+      render(<Catalog props="Rod" />);
+      expect(screen.getAllByTestId('card')).toHaveLength(1);
+    });
+  });
+//   In this example, the first test ensures that the component renders without errors by checking if the catalog-container element is in the document. The second test checks if all cards are displayed when no search query is provided, and the third test checks if the cards are filtered correctly based on the search query "Rod".
+  
+//   To run these tests, you can use the npm test command. Jest will automatically find and run all test files in your project.
+  
+//   Note that this is just an example, and you'll need to customize the tests to match the specific behavior and requirements of your application. Additionally, you may want to consider using a testing library like Enzyme or Cypress to add more advanced testing capabilities to your project.
+  
+  
+  
+  
+  
+  
