7df365e94044f6be8106b1826ef7b460dcab4a96
instamenta <98179343+instamenta@users.noreply.github.com>
2023-03-18 01:52:08 +0200

    suusususu

--- react-app/src/setupTests.js
+++ react-app/src/setupTests.js
@@ -1,1 +1,9 @@
+const enzyme = require('enzyme');
+// const Adapter = require('@wojtekmaj/enzyme-adapter-react-17');
+const { configure } = require('enzyme-to-json');
+
+configure({ adapter: new Adapter() });
+
+const toJson = require('enzyme-to-json').default;
+
 require('@testing-library/jest-dom/extend-expect');
