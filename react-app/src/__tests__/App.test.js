b4bcf05cf9823b20c34bcc5d4acb191177e8e14e
instamenta <98179343+instamenta@users.noreply.github.com>
2023-03-17 15:32:23 +0200

    sus

---
+++ react-app/src/__tests__/App.test.js
@@ -0,0 +1,18 @@
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import React from 'react';
+import App from './App';
+import { Catalog } from './components/Catalog/Catalog';
+
+describe('App', () => {
+  it('renders the search box', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('.search-box')).to.have.lengthOf(1);
+  });
+
+  it('passes the search value to the Catalog component', () => {
+    const wrapper = shallow(<App />);
+    const catalog = wrapper.find(Catalog);
+    expect(catalog.prop('searchValue')).to.equal(wrapper.state('searchValue'));
+  });
+});
